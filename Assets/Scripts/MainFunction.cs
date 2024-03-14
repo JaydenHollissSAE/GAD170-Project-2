5a3704bcadab5576f5acc943b3bc89cd82e5a1e1
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-03-14 11:57:50 +1100

    Made Functional

--- Assets/Scripts/MainFunction.cs
+++ Assets/Scripts/MainFunction.cs
@@ -5,33 +5,32 @@ using UnityEngine;
 
 public class MainFunction : MonoBehaviour
 {
-    public string[] plantsList = new string[] { };
-    public float[] plantsListCost = new float[] { };
+    public List<string> plantsList = new List<string>();
+    public List<float> plantsListCost = new List<float>();
     public int selectedPlant;
     public float funds;
     public float totalFunds;
     public float plantCost;
     public string plantName;
-    public int plantAmounts;
-    public int plantCapacity;
+    public int plantCapacity = 5;
     public string[] namesList = new string[] { "Flower", "Cabbage", "Carrot", "Tulip", "Bush", "Cherry Tree", "Apple Tree", "Vine", "Rose" };
     public string[] descriptionList = new string[] {"Luscious", "Royal", "Draconic", "Carnivorous", "Frozen", "Preserved", "Lovely", "Fresh", "Tasty"};
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddPlant();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void AddPlant()
     {
-        if (plantAmounts < plantCapacity)
+        if (plantsList.Count < plantCapacity)
         {
-            plantName = descriptionList[Random.Range(0, descriptionList.Length)] + namesList[Random.Range(0,namesList.Length)];
+            plantName = descriptionList[Random.Range(0, descriptionList.Length)] + " " + namesList[Random.Range(0,namesList.Length)];
             plantCost = Random.Range(1f, 10f);
             plantsList.Add(plantName);
             plantsListCost.Add(plantCost);
@@ -41,10 +40,9 @@ public class MainFunction : MonoBehaviour
             SellPlant();
         }
     }
-
     public void SellPlant()
     {
-        selectedPlant = Random.Range(0, plantsList.Length);
+        selectedPlant = Random.Range(0, plantsList.Count);
         
 
         plantsList.RemoveAt(selectedPlant);
